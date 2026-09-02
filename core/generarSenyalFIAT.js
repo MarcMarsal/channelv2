8d37bdcb652f01b92b169ec8ab2b433b3d9d4c6d
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-09-02 19:17:36 +0200

    Refactor generarSenyalFIAT to include date and time

--- core/generarSenyalFIAT.js
+++ core/generarSenyalFIAT.js
@@ -1,16 +1,37 @@
-// generarSenyalFIAT
-
 import { client } from "../db/client.js";
+import { formatSpainDate, formatSpainTime } from "./utils.js";
 
 export async function generarSenyalFIAT(symbol, timestamp, accio, open, close, upper, lower) {
+
+    const date_es = formatSpainDate(timestamp);
+    const hora_es = formatSpainTime(timestamp);
+
     await client.query(`
-        INSERT INTO signals_fiat (
-            symbol, timestamp, accio, open, close, upper, lower, created_at
+        INSERT INTO signals_channels (
+            symbol,
+            timeframe,
+            type,
+            entry,
+            tp,
+            sl,
+            timestamp,
+            timestamp_ms,
+            date_es,
+            hora_es,
+            created_at
         ) VALUES (
-            $1, $2, $3, $4, $5, $6, $7,
+            $1, '15m', $2, $3, $4, $5,
+            $6, $6, $7, $8,
             EXTRACT(EPOCH FROM NOW()) * 1000
         )
     `, [
-        symbol, timestamp, accio, open, close, upper, lower
+        symbol,
+        accio,
+        open,
+        upper,
+        lower,
+        timestamp,
+        date_es,
+        hora_es
     ]);
 }
